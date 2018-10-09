98b8d698a5358e9072ad7c4ada0fd8b985d961d1
Matthew Freeman <matthew.freeman821@gmail.com>
2018-10-09 14:22:09 -0400

    Added show route, added show template, added links to show page, styled show template

--- app.js
+++ app.js
@@ -58,6 +58,17 @@ app.post('/blogs', function(req, res){
     //redirect to the index
 });
 
+// SHOW ROUTE
+app.get('/blogs/:id', function(req, res){
+    Blog.findById(req.params.id, function(err, foundBlog) {
+        if(err) {
+            res.redirect('/blogs');
+        } else {
+            res.render('show', {blog: foundBlog});
+        }
+    });
+});
+
 app.listen(4000, function(){
     console.log('Server running on localhost 4000')
 });

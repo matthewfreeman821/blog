fd1005e619659e051116b7099a97a1940e280c17
Matthew Freeman <matthew.freeman821@gmail.com>
2018-10-09 16:31:10 -0400

    sanitized blog body, styled index, updated rest table

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 var bodyParser = require('body-parser'),
     methodOverride = require('method-override'),
+    expressSanitizer = require('express-sanitizer'),
     mongoose = require('mongoose'),
     express = require('express'),
     app = express();
@@ -9,6 +10,7 @@ mongoose.connect('mongodb://localhost/bloggyblog');
 app.set('view engine', 'ejs');
 app.use(express.static('public'));
 app.use(bodyParser.urlencoded({extended: true}));
+app.use(expressSanitizer());
 app.use(methodOverride("_method"));
 
 //Mongoose/Model Config
@@ -50,6 +52,7 @@ app.get('/blogs/new', function(req, res){
 // CREATE ROUTE
 app.post('/blogs', function(req, res){
     //create blog
+    req.body.blog.body = req.sanitize(req.body.blog.body);
     Blog.create(req.body.blog, function(err, newBlog){
         if(err){
             res.render('new');
@@ -84,6 +87,7 @@ app.get('/blogs/:id/edit', function(req, res) {
 
 //UPDATE ROUTE
 app.put('/blogs/:id', function(req, res) {
+    req.body.blog.body = req.sanitize(req.body.blog.body);
     Blog.findOneAndUpdate(req.param.id, req.body.blog, function(err, updatedBlog) {
         if(err) {
             res.redirect('/blogs');
